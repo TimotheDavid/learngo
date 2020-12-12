96c1f2db58f03f02dc93fe3151b853fc6f364d07
TimotheDavid <timoth.david@gmail.com>
2020-12-12 15:20:32 +0100

    value nominal return by the fnction himself

--- helloworld.go
+++ helloworld.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
+func split(sum int) (x , y int) {
+	x = sum * 3 / 10
+	y = sum - x
+	// return var nominal : sum 
+	return 
+}
+
+
+// function who go two parameter of int and return a int 
+func add(x, y int) int {
+	return x + y
+}
+
+// main function mandatory
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Hello world", rand.Intn(30))
+	fmt.Println(add(20,50))
 
 
 }
